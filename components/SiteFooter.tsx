8df1343a3af35dd0ba5e68a9b4d07bfe38c43a26
ahsan-official82 <ahsan.official82@gmail.com>
2026-04-13 19:03:52 -0700

    Update Footer

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 export function SiteFooter() {
+  const year = new Date().getFullYear();
   return (
     <footer id="footer" className="footer position-relative light-background">
       <div className="container">
         <div className="copyright text-center">
-          <p>© 2025 Ahmer Mairaj. All rights reserved.</p>
+          <p>© {year} Ahmer Mairaj. All rights reserved.</p>
         </div>
       </div>
     </footer>
