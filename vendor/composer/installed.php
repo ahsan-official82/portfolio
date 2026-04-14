63ab78d69e112a2268373fc44f4f2aece6b13c44
ahsan-official82 <ahsan.official82@gmail.com>
2026-04-13 18:55:01 -0700

    Move assets

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'portfolio/contact-mail',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0941c5513481b45eda682b65c1d8a7339599dab6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v6.12.0',
+            'version' => '6.12.0.0',
+            'reference' => 'd1ac35d784bf9f5e61b424901d5a014967f15b12',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'portfolio/contact-mail' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0941c5513481b45eda682b65c1d8a7339599dab6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
